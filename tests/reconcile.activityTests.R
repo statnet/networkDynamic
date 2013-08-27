b0d60ebafddeeae33b41f97f48e56c3bf0fb098d
Li Wang <lxwang@uw.edu>
2013-08-27 08:11:40 +0000

    Implemented functions according to ticket 71 (Trac: 169). Tests and docs included!
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/networkDynamic/trunk/networkDynamic@12170 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- tests/reconcile.activityTests.R
+++ tests/reconcile.activityTests.R
@@ -11,36 +11,190 @@ nd<-network.initialize(6)
 add.edges.active(nd,tail=1:3,head=2:4,onset=1,terminus=3)
 add.edges.active(nd,tail=4,head=1,onset=5,terminus=7)
 add.edge(nd,tail=1,head=6)
+deactivate.edges(nd, onset=-Inf, terminus=Inf, e=get.edgeIDs(nd, v=1, alter=6))
+add.edge(nd,tail=1, head=5)  # default activated
 nd2<-reconcile.vertex.activity(nd)
 spls<-get.vertex.activity(nd2,as.spellList=TRUE)
-expect_equal(spls$onset,c(1,1,1,1,-Inf))
-expect_equal(spls$terminus,c(3,3,3,3,Inf))
-expect_equal(spls$vertex.id,c(1,2,3,4,6))
+expect_equal(spls$onset,c(-Inf, 1,1,1,5,-Inf))
+expect_equal(spls$terminus,c(Inf,3,3,3,7,Inf))
+expect_equal(spls$vertex.id,c(1,2,3,4,4,5))
 
 # test modifiy in place
 nd<-network.initialize(6)
 add.edges.active(nd,tail=1:3,head=2:4,onset=1,terminus=3)
 add.edges.active(nd,tail=4,head=1,onset=5,terminus=7)
 add.edge(nd,tail=1,head=6)
+deactivate.edges(nd, onset=-Inf, terminus=Inf, e=get.edgeIDs(nd, v=1, alter=6))
+add.edge(nd,tail=1, head=5)  # default activated
 reconcile.vertex.activity(nd)
 spls<-get.vertex.activity(nd,as.spellList=TRUE)
-expect_equal(spls$onset,c(1,1,1,1,-Inf),info='reconcile.vertex.activity modify in place')
-expect_equal(spls$terminus,c(3,3,3,3,Inf),info='reconcile.vertex.activity modify in place')
-expect_equal(spls$vertex.id,c(1,2,3,4,6),info='reconcile.vertex.activity modify in place')
+expect_equal(spls$onset,c(-Inf, 1,1,1,5,-Inf),info='reconcile.vertex.activity modify in place')
+expect_equal(spls$terminus,c(Inf,3,3,3,7,Inf),info='reconcile.vertex.activity modify in place')
+expect_equal(spls$vertex.id,c(1,2,3,4,4,5),info='reconcile.vertex.activity modify in place')
 
 # test with edge active default FALSE
 nd<-network.initialize(6)
 add.edges.active(nd,tail=1:3,head=2:4,onset=1,terminus=3)
 add.edges.active(nd,tail=4,head=1,onset=5,terminus=7)
 add.edge(nd,tail=1,head=6)
-reconcile.vertex.activity(nd,edge.active.default=FALSE)
-spls<-get.vertex.activity(nd,as.spellList=TRUE)
-expect_equal(spls$onset,c(1,1,1,1))
-expect_equal(spls$terminus,c(3,3,3,3))
-expect_equal(spls$vertex.id,c(1,2,3,4))
+deactivate.edges(nd, onset=-Inf, terminus=Inf, e=get.edgeIDs(nd, v=1, alter=6))
+add.edge(nd,tail=1, head=5)  # default activated
+nd2<-reconcile.vertex.activity(nd, edge.active.default=F)
+spls<-get.vertex.activity(nd2,as.spellList=TRUE)
+expect_equal(spls$onset,c(1, 5, 1, 1, 1, 5))
+expect_equal(spls$terminus,c(3, 7, 3, 3, 3, 7))
+expect_equal(spls$vertex.id,c(1, 1, 2, 3, 4, 4))
 
 # test with bad mode
 expect_error(reconcile.vertex.activity(nd,mode='foobar'),'not supported')
 
+# ---- reconcile.vertex.activity tests (expand.to.edges mode) ------------
+
+nd<-network.initialize(6)
+deactivate.vertices(nd, onset=-Inf, terminus=Inf)
+activate.vertices(nd, v=2, onset=2, terminus=5)
+add.edges.active(nd,tail=1:3,head=2:4,onset=1,terminus=3)
+add.edges.active(nd,tail=4,head=1,onset=5,terminus=7)
+add.edge(nd,tail=1,head=6)
+deactivate.edges(nd, onset=-Inf, terminus=Inf, e=get.edgeIDs(nd, v=1, alter=6))
+add.edge(nd,tail=1, head=5)  # default activated
+nd2<-reconcile.vertex.activity(nd, mode='expand.to.edges')
+spls<-get.vertex.activity(nd2,as.spellList=TRUE)
+expect_equal(spls$onset,c(-Inf, 1,1,1,5,-Inf))
+expect_equal(spls$terminus,c(Inf,5,3,3,7,Inf))
+expect_equal(spls$vertex.id,c(1,2,3,4,4,5))
+
+nd<-network.initialize(6)
+deactivate.vertices(nd, onset=-Inf, terminus=Inf)
+activate.vertices(nd, v=2, onset=5, terminus=7)
+add.edges.active(nd,tail=1:3,head=2:4,onset=1,terminus=3)
+add.edges.active(nd,tail=4,head=1,onset=5,terminus=7)
+add.edge(nd,tail=1,head=6)
+deactivate.edges(nd, onset=-Inf, terminus=Inf, e=get.edgeIDs(nd, v=1, alter=6))
+add.edge(nd,tail=1, head=5)  # default activated
+nd2<-reconcile.vertex.activity(nd, mode='expand.to.edges')
+spls<-get.vertex.activity(nd2,as.spellList=TRUE)
+expect_equal(spls$onset,c(-Inf, 1,5,1,1,5,-Inf))
+expect_equal(spls$terminus,c(Inf,3,7,3,3,7,Inf))
+expect_equal(spls$vertex.id,c(1,2,2,3,4,4,5))
+
+# ---- reconcile.vertex.activity tests (encompass.edges mode) ------------
+
+nd<-network.initialize(6)
+deactivate.vertices(nd, onset=-Inf, terminus=Inf)
+activate.vertices(nd, v=2, onset=2, terminus=5)
+add.edges.active(nd,tail=1:3,head=2:4,onset=1,terminus=3)
+add.edges.active(nd,tail=4,head=1,onset=5,terminus=7)
+add.edge(nd,tail=1,head=6)
+deactivate.edges(nd, onset=-Inf, terminus=Inf, e=get.edgeIDs(nd, v=1, alter=6))
+add.edge(nd,tail=1, head=5)  # default activated
+nd2<-reconcile.vertex.activity(nd, mode='encompass.edges')
+spls<-get.vertex.activity(nd2,as.spellList=TRUE)
+# combines the spells for vertex 4
+expect_equal(spls$onset,c(-Inf, 1,1,1,-Inf))
+expect_equal(spls$terminus,c(Inf,3,3,7,Inf))
+expect_equal(spls$vertex.id,c(1,2,3,4,5))
+
+# testing 0-duration spells
+nd<-network.initialize(6)
+deactivate.vertices(nd, onset=-Inf, terminus=Inf)
+activate.vertices(nd, v=2, onset=2, terminus=5)
+add.edges.active(nd,tail=1:2,head=2:3,onset=1,terminus=3)
+
+add.edges.active(nd,tail=4,head=1,onset=9,terminus=9)
+add.edge(nd,tail=1,head=6)
+deactivate.edges(nd, onset=-Inf, terminus=Inf, e=get.edgeIDs(nd, v=1, alter=6))
+add.edge(nd,tail=1, head=5)  # default activated
+activate.edges(nd, e=2, at=3)
+#get.edge.activity(nd, as.spell=T)
+#get.vertex.activity(nd, as.spell=T)
+nd2<-reconcile.vertex.activity(nd, mode='encompass.edges')
+spls<-get.vertex.activity(nd2,as.spellList=TRUE)
+# combines the spells for vertex 4
+expect_equal(spls$onset,c(-Inf, 1,1,9,-Inf))
+expect_equal(spls$terminus,c(Inf,4,4,9,Inf))
+expect_equal(spls$vertex.id,c(1,2,3,4,5))
 
 
+# ---- reconcile.edge.activity tests (reduce.to.vertices mode) ------------
+nd<- network.initialize(6)
+activate.vertices(nd, onset=1, terminus=2)
+add.edges.active(nd,tail=1:3,head=2:4,onset=1,terminus=3)
+add.edges.active(nd,tail=4,head=1,onset=5,terminus=7)
+add.edge(nd,tail=1,head=6)
+deactivate.edges(nd, onset=-Inf, terminus=Inf, e=get.edgeIDs(nd, v=1, alter=6))
+add.edge(nd,tail=1, head=5)  # default activated
+#get.edge.activity(nd, as.spell=T)
+#get.vertex.activity(nd, as.spell=T)
+nd2<-reconcile.edge.activity(nd, mode='reduce.to.vertices')
+spls<-get.edge.activity(nd2,as.spellList=TRUE)
+expect_equal(spls$onset,c(1,1,1,1))
+expect_equal(spls$terminus,c(2,2,2,2))
+expect_equal(spls$edge.id,c(1,2,3,6))
+
+# censored spells
+nd<- network.initialize(6)
+activate.vertices(nd, onset=2, terminus=Inf)
+add.edges.active(nd,tail=1:3,head=2:4,onset=1,terminus=3)
+add.edges.active(nd,tail=4,head=1,onset=5,terminus=7)
+add.edge(nd,tail=1,head=6)
+deactivate.edges(nd, onset=-Inf, terminus=Inf, e=get.edgeIDs(nd, v=1, alter=6))
+add.edge(nd,tail=1, head=5)  # default activated
+#get.edge.activity(nd, as.spell=T)
+#get.vertex.activity(nd, as.spell=T)
+nd2<-reconcile.edge.activity(nd, mode='reduce.to.vertices')
+spls<-get.edge.activity(nd2,as.spellList=TRUE)
+expect_equal(spls$onset,c(2,2,2,5,2))
+expect_equal(spls$terminus,c(3,3,3,7,Inf))
+expect_equal(spls$edge.id,c(1,2,3,4,6))
+
+# 0-duration spells
+nd<- network.initialize(6)
+activate.vertices(nd, onset=1, terminus=2)
+activate.vertices(nd, at=6)
+add.edges.active(nd,tail=1:3,head=2:4,onset=1,terminus=3)
+add.edges.active(nd,tail=4,head=1,onset=5,terminus=7)
+add.edge(nd,tail=1,head=6)
+deactivate.edges(nd, onset=-Inf, terminus=Inf, e=get.edgeIDs(nd, v=1, alter=6))
+add.edge(nd,tail=1, head=5)  # default activated
+#get.edge.activity(nd, as.spell=T)
+#get.vertex.activity(nd, as.spell=T)
+nd2<-reconcile.edge.activity(nd, mode='reduce.to.vertices')
+spls<-get.edge.activity(nd2,as.spellList=TRUE)
+expect_equal(spls$onset,c(1,1,1,6,1,6))
+expect_equal(spls$terminus,c(2,2,2,6,2,6))
+expect_equal(spls$edge.id,c(1,2,3,4,6,6))
+
+
+# ---- reconcile.edge.activity tests (match.to.vertices mode) ------------
+nd<- network.initialize(6)
+activate.vertices(nd, onset=1, terminus=2)
+add.edges.active(nd,tail=1:3,head=2:4,onset=1,terminus=3)
+add.edges.active(nd,tail=4,head=1,onset=5,terminus=7)
+add.edge(nd,tail=1,head=6)
+deactivate.edges(nd, onset=-Inf, terminus=Inf, e=get.edgeIDs(nd, v=1, alter=6))
+add.edge(nd,tail=1, head=5)  # default activated
+get.edge.activity(nd, as.spell=T)
+get.vertex.activity(nd, as.spell=T)
+nd2<-reconcile.edge.activity(nd, mode='match.to.vertices')
+spls<-get.edge.activity(nd2,as.spellList=TRUE)
+expect_equal(spls$onset,c(1,1,1,1,1,1))
+expect_equal(spls$terminus,c(2,2,2,2,2,2))
+expect_equal(spls$edge.id,c(1,2,3,4,5,6))
+
+# 0-duration spells
+nd<- network.initialize(6)
+activate.vertices(nd, onset=1, terminus=2, v=c(1,2,3))
+deactivate.vertices(nd, v=4, onset=-Inf, terminus=Inf)
+activate.vertices(nd, at=6, v=c(1,6))
+add.edges.active(nd,tail=1:3,head=2:4,onset=1,terminus=3)
+add.edges.active(nd,tail=4,head=1,onset=5,terminus=7)
+add.edge(nd,tail=1,head=6)
+add.edge(nd,tail=1, head=5)  # default activated
+#get.edge.activity(nd, as.spell=T)
+#get.vertex.activity(nd, as.spell=T)
+nd2<-reconcile.edge.activity(nd, mode='match.to.vertices')
+spls<-get.edge.activity(nd2,as.spellList=TRUE)
+expect_equal(spls$onset,c(1,1,6,1,6))
+expect_equal(spls$terminus,c(2,2,6,2,6))
+expect_equal(spls$edge.id,c(1,2,5,6,6))
