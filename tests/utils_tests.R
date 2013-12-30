83687b87eded93d58dd1b938bd4bb1ee001596b3
Skye Bender-deMoll <skyebend@uw.edu>
2013-12-30 22:46:32 +0000

    implemented a get.dyads.active function for #154 (Trac: 652) with docs and tests
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/networkDynamic/trunk@12416 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- tests/utils_tests.R
+++ tests/utils_tests.R
@@ -53,3 +53,39 @@ add.vertices.active(net,nv=2, onset=1,terminus=2)
 expect_equal(network.size(net),5)
 expect_true(is.networkDynamic(net))
 expect_equal(unlist(get.vertex.activity(net,as.spellList=TRUE)[4:5,1:2]),c(1,1,2,2),check.names=FALSE)
+
+
+# ---- tests for get.dyads.active ---
+test_that({
+  
+  expect_error( get.dyads.active(network.initialize(3,hyper=TRUE),at=1),"does not currently support hypergraphic","error on hyper")
+  expect_equal( nrow(get.dyads.active(network.initialize(0),at=1)),0,"network size zero case")
+  expect_equal( nrow(get.dyads.active(network.initialize(3),at=1)),0,"zero edges case")
+  
+  test<-network.initialize(5)
+  add.edges.active(test,tail=1,head=2,onset=0,terminus=1)
+  add.edges.active(test,tail=2,head=3,onset=1,terminus=2)
+  add.edges.active(test,tail=3,head=4,onset=1,terminus=3)
+  activate.edges(test,e=1,onset=2,terminus=3)
+  as.data.frame(test)
+  expect_equal(get.dyads.active(test,at=0),rbind(1:2))
+  expect_equal(get.dyads.active(test,at=1),cbind(2:3,3:4))
+  expect_equal(get.dyads.active(test,onset=0,terminus=4),rbind(1:2,2:3,3:4))
+  
+  # test with no (default) dynamics
+  test2<-network.initialize(3)
+  test2[1,2]<-1
+  expect_equal(get.dyads.active(test2,onset=0,terminus=4),rbind(1:2))
+  # test active default
+  expect_equal(get.dyads.active(test2,onset=0,terminus=4,active.default=FALSE),matrix(numeric(),ncol=2,nrow=0),check.attributes=FALSE, 'test active default arg')
+  
+  # deleted edges
+  test2<-network.initialize(3)
+  test2[1,2]<-1
+  test2[2,3]<-1
+  test2[3,1]<-1
+  delete.edges(test2,eid=2)
+  expect_equal(get.dyads.active(test2,at=1),rbind(1:2,c(3,1)),'deleted edge case')
+  
+},"test get.dyads.active")
+
