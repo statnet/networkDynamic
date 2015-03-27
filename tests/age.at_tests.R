ab6823f17f36ab613e7ab152cb21761dbd675529
Skye Bender-deMoll <skyebend@uw.edu>
2015-03-27 20:41:50 +0000

    explicit S3 method registration, updates to dyad.ages.at to optionally format output as edgelist or adjacency matrix
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/networkDynamic/trunk/networkDynamic@13647 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- tests/age.at_tests.R
+++ tests/age.at_tests.R
@@ -60,6 +60,22 @@ expect_equal(dyads.age.at(test,at=1,tail=1:2,head=2:3),c(NA,NA))
 expect_equal(dyads.age.at(test,at=0,tail=4,head=5),Inf)
 expect_equal(dyads.age.at(test,at=0,tail=4,head=5,active.default=FALSE),NA)
 
+# test edge format arge
+expect_equal(dyads.age.at(test,at=0),c(0,NA,Inf))
+out<-dyads.age.at(test,at=0,format.out='edgelist')
+expect_equal(out[,'tails'],c(1,3,4))
+expect_equal(out[,'heads'],c(2,4,5))
+expect_equal(out[,'ages'],c(0,NA,Inf))
+
+out<-dyads.age.at(test,at=0,format.out='matrix')
+expect_equal(out,matrix(c(NA,  NA,  NA,  NA,  NA,   0,  NA,  NA,  NA,  NA,  NA,  NA,  NA,  NA,  NA,  NA,  NA,  NA,  NA,  NA,  NA,  NA,  NA, Inf, NA),nrow=5,ncol=5))
+
+# test symmetry for non directed case
+test%n%'directed'<-FALSE
+out<-dyads.age.at(test,at=0,format.out='matrix')
+expect_equal(out,matrix(c(NA,   0,  NA,  NA,  NA,   0,  NA,  NA,  NA,  NA,  NA,  NA,  NA,  NA,  NA,  NA,  NA,  NA,  NA, Inf,  NA,  NA,  NA, Inf,  NA),nrow=5,ncol=5))
+
+
 # ----- test vertices.age.at -----
 test<-network.initialize(5)
 activate.vertices(test,v = 1:4,onset=0:3,terminus=1:4)
